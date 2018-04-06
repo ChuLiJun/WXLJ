90c22b7077d0bc9b7098abad2ef0a19dc8d94e76
ChuLiJun <1994703260@qq.com>
2018-04-06 22:24:22 +0800

    Update ViewController.m

--- wxPayDemo/ViewController.m
+++ wxPayDemo/ViewController.m
@@ -19,7 +19,7 @@
     /*
      第一步   导入 SDK     pod 'WechatOpenSDK'
      第二步   配置白名单    LSApplicationQueriesSchemes
-     第三步   配置URL schemes   wx251d0396a1ab7e85  //微信开放平台获取
+     第三步   配置URL schemes   wx251d0396a1ab***  //微信开放平台获取
      第四步。  appDelegate.m 导入头文件 遵循代理 设置回调和通知监听 回调结果
     
     */
